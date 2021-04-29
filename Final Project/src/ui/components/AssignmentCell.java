fbac8123e1e76677f25fa0ef3b6728cbf430e023
dnnnnn0626 <77896070+dnnnnn0626@users.noreply.github.com>
2021-04-28 23:41:25 -0500

    Add files via upload

--- Final Project/src/ui/components/AssignmentCell.java
+++ Final Project/src/ui/components/AssignmentCell.java
@@ -53,10 +53,14 @@ public class AssignmentCell extends javax.swing.JPanel {
             }
         });
 
-        jPanel2.setBackground(new java.awt.Color(177, 177, 248));
+        jPanel2.setBackground(new java.awt.Color(160, 190, 252));
         jPanel2.setPreferredSize(new java.awt.Dimension(320, 240));
 
-        titleLabel.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(191, 210, 251));
+
+        titleLabel.setBackground(new java.awt.Color(188, 209, 251));
+        titleLabel.setFont(new java.awt.Font("STIXNonUnicode", 1, 16)); // NOI18N
+        titleLabel.setForeground(new java.awt.Color(51, 102, 255));
         titleLabel.setText("jLabel1");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -87,7 +91,7 @@ public class AssignmentCell extends javax.swing.JPanel {
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(0, 147, Short.MAX_VALUE)
+                .addGap(0, 15, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
@@ -114,11 +118,11 @@ public class AssignmentCell extends javax.swing.JPanel {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(12, 12, 12))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
